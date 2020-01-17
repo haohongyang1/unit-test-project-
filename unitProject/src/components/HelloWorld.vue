537bb02a4f54806dfc34e7acbfac718c525df31f
haohongyang <haohongyang@vipkid.com.cn>
2020-01-17 14:46:10 +0800

    fn finish

---
+++ unitProject/src/components/HelloWorld.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="hello">
+    <div class="">
+      <button type="button" name="button" @click="attemptResult()">attempt</button>
+    </div>
+    <textarea name="name" rows="300" cols="80"></textarea>
+    <textarea v-model="resultShow" name="name" rows="300" cols="80"></textarea>
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      resultShow: '',
+      codeDetails: ''
+    }
+  },
+  methods: {
+    attemptResult () {
+      this.resultShow = process.on('warning', (warning) => {
+        console.warn(warning.name)
+        console.warn(warning.message) // 'Something happened!'
+        console.warn(warning.code) // 'MY_WARNING'
+        console.warn(warning.stack) // Stack trace
+        console.warn(warning.detail) // 'This is some additional information'
+      })
+      console.log(this.resultShow)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
